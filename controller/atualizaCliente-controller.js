b8e1470ad3c88b6ab5e5582cb8135312d5b9f7ee
Max Junior <maxjunior20012300@gmail.com>
2023-01-25 21:52:02 -0300

    Tratando erros com try catch

--- controller/atualizaCliente-controller.js
+++ controller/atualizaCliente-controller.js
@@ -8,9 +8,15 @@ const id = pegaURL.searchParams.get('id');
 const inputNome = document.querySelector('[data-nome]');
 const inputEmail = document.querySelector('[data-email]');
 
-const dados = await clienteService.detalhaCliente(id)
-inputNome.value = dados.nome;
-inputEmail.value = dados.email;
+try{
+    const dados = await clienteService.detalhaCliente(id)
+    inputNome.value = dados.nome;
+    inputEmail.value = dados.email;
+}
+catch(erro){
+    console.log(erro);
+    window.location.href = '../telas/erro.html';
+}
 
 
 const formulario = document.querySelector('[data-form]');
@@ -18,8 +24,14 @@ const formulario = document.querySelector('[data-form]');
 formulario.addEventListener('submit', async (evento)=>{
     evento.preventDefault();
 
-    await clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
-    window.location.href = '../telas/edicao_concluida.html';
+    try{
+        await clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
+        window.location.href = '../telas/edicao_concluida.html';
+    }
+    catch(erro){
+        console.log(erro);
+        window.location.href = '../telas/erro.html';
+    }
 })
 })()
 
